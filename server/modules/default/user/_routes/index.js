a62f09bef6e9122eca8ae0008725338c89279429
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2017-09-16 16:36:32 +0100

    Removed uuid's import from the user index route.

--- server/modules/default/user/_routes/index.js
+++ server/modules/default/user/_routes/index.js
@@ -1,14 +1,12 @@
 'use strict'
 
 import Router from 'koa-trie-router'
-import uuidv1 from 'uuid/v1'
 import controller from '../controllers'
 
 const router = new Router()
 
 export default () => {
   router.get('/', async (ctx, next) => {
-    console.log(uuidv1())
     let result
     try {
       result = await controller(ctx, next)
